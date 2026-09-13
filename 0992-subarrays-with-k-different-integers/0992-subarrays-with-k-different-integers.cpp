03550782a799f7644eb08f7d2620d3bb47c7d349
Sahejpal Singh Kataroda <sahejsingh.jinp@gmail.com>
2026-09-13 22:10:07 +0530

    Time: 87 ms (43.89%), Space: 56.3 MB (39.33%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atmost(nums, k) - atmost(nums, k-1);
+    }
+
+    int atmost(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int left = 0, count = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            mp[nums[right]]++;
+
+            while (mp.size() > k) {
+                mp[nums[left]]--;
+                if (mp[nums[left]] == 0) {
+                    mp.erase(nums[left]);
+                }
+                left++;
+            }
+            count += right - left + 1;
+        }
+        return count;
+    }
+};
