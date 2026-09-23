c3877b4dcbc85c63d59315043953bbfbb72ece50
Sahejpal Singh Kataroda <sahejsingh.jinp@gmail.com>
2026-09-23 19:21:19 +0530

    Time: 7 ms (99.96%), Space: 8.8 MB (88.88%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int n = s.size();
-        int total = 0;
-
-        for (int i = 0; i < n; i++) {
-            vector<int> freq(26, 0);
-
-            for (int j = i; j < n; j++) {
-                // Track frequency of characters in substring s[i..j]
-                freq[s[j] - 'a']++;
-
-                int maxi = 0;
-                int mini = INT_MAX;
-
-                // Find max and min non-zero frequency in current substring
-                for (int k = 0; k < 26; k++) {
-                    if (freq[k] > 0) {
-                        maxi = max(maxi, freq[k]);
-                        mini = min(mini, freq[k]);
+        int n=s.length();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int mf=1;
+            int lf=1;
+            int freq[26]={0};
+            freq[s[i]-'a']++;
+            int clf=1;
+            for(int j=i+1;j<n;j++){
+                int currFreq=++freq[s[j]-'a'];
+                if(currFreq>mf) mf=currFreq;
+                if(currFreq<lf){
+                    lf=currFreq;
+                    clf=1;
+                }
+                else if(currFreq==lf) clf++;
+                else if(currFreq==lf+1 && --clf==0){
+                    lf++;
+                    for(int k=0;k<26;k++){
+                        if(lf==freq[k]) clf++;
                     }
                 }
-
-                // Add the beauty of substring s[i..j] to running total
-                total += (maxi - mini);
+                if(mf-lf!=0) ans+=mf-lf; 
             }
         }
-
-        return total;
+        return ans;
     }
-};
+}; 
